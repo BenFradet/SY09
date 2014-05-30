e717d283ed2e5c2b0e8a00f7c2277d327e06540b
BenFradet <benjamin.fradet@gmail.com>
2014-05-30 15:21:27 +0200

    ex1 done

--- TP4/ex1.R
+++ TP4/ex1.R
@@ -1,8 +1,7 @@
 library(MASS)
 #library(klaR)
 
-doStuff <- function(filename) {
-    data <- read.table(filename)
+graph <- function(data, filename) {
     len <- dim(data)[1]
     lda <- lda(data[,c(1, 2)], data$Classe) 
     qda <- qda(data[,c(1,2)], data$Classe)
@@ -31,7 +30,7 @@ doStuff <- function(filename) {
            col = c("red", "blue"),
            pch = c(1, 2),
            cex = 0.8)
-    legend(max(data[,1]) + 2, max(data[,2]) - 4,
+    legend(max(data[,1]) + 2, max(data[,2]) - 6,
            c("Frontiere de decision\nissue de la lda\n",
              "Frontiere de decision\nissue de la qda"),
            col = c("skyblue", "orange"),
@@ -52,31 +51,105 @@ doStuff <- function(filename) {
     par(mar = c(5, 4, 4, 2) + 0.1)
     dev.off()
     cat(paste(pngname, "sauvegardee\n"))
-
-    #probas d'erreur
-    data <- cbind(data, predict(lda, data[,c(1,2)])$class)
-    colnames(data)[4] <- "predictedLDA"
-
-    data <- cbind(data, predict(qda, data[,c(1,2)])$class)
-    colnames(data)[5] <- "predictedQDA"
-
-    probaLDA <- sum(apply(data, 1,
-                          function(row) {
-                              if(row["Classe"] != row["predictedLDA"]) {
-                                  return(1)
-                              }
-                              return(0)
-                          })) / len
-    probaQDA <- sum(apply(data, 1,
-                          function(row) {
-                              if(row["Classe"] != row["predictedQDA"]) {
-                                  return(1)
-                              }
-                              return(0)
-                          })) / len
-    cat(filename, "proba lda:", probaLDA, ", proba qda:", probaQDA, "\n")
 }
 
+probaErreur <- function(data, trainingRatio, pi1, pi2) {
+    #probas d'erreur
+    len <- as.integer(dim(data)[1] * trainingRatio)
+    index <- sample(nrow(data), len)
+    training <- data[index,] 
+    test <- data[-index,]
+
+    lda <- lda(data[,c(1,2)], data$Classe, 
+               prior = c(pi1, pi2), 
+               subset = index)
+    qda <- qda(data[,c(1,2)], data$Classe,
+               prior = c(pi1, pi2),
+               subset = index)
+    
+    training <- cbind(training, predict(lda, training[,c(1,2)])$class)
+    colnames(training)[4] <- "predictedLDA"
+    training <- cbind(training, predict(qda, training[,c(1,2)])$class)
+    colnames(training)[5] <- "predictedQDA"
+
+    if(len != dim(data)[1]) {
+        test <- cbind(test, predict(lda, test[,c(1,2)])$class)
+        colnames(test)[4] <- "predictedLDA"
+        test <- cbind(test, predict(qda, test[,c(1,2)])$class)
+        colnames(test)[5] <- "predictedQDA"
+    }
+
+    probaTrainingLDA <- sum(apply(training, 1,
+                                  function(row) {
+                                      if(row["Classe"] != row["predictedLDA"]) {
+                                          return(1)
+                                      }
+                                      return(0)
+                                  })) / dim(training)[1]
+    probaTrainingQDA <- sum(apply(training, 1,
+                                  function(row) {
+                                      if(row["Classe"] != row["predictedQDA"]) {
+                                          return(1)
+                                      }
+                                      return(0)
+                                  })) / dim(training)[1]
+
+    if(len != dim(data)[1]) {
+        probaTestLDA <- sum(apply(test, 1,
+                                  function(row) {
+                                      if(row["Classe"] != row["predictedLDA"]) {
+                                          return(1)
+                                      }
+                                      return(0)
+                                  })) / dim(test)[1]
+        probaTestQDA <- sum(apply(test, 1,
+                                  function(row) {
+                                      if(row["Classe"] != row["predictedQDA"]) {
+                                          return(1)
+                                      }
+                                      return(0)
+                                  })) / dim(test)[1]
+    } else {
+        probaTestLDA <- 0
+        probaTestQDA <- 0
+    }
+    return(c(probaTrainingLDA, probaTrainingQDA, probaTestLDA, probaTestQDA))
+}
+
+data <- c()
 for(i in 1:6) {
-    doStuff(paste("data",i,".txt", sep = ""))
+    filename <- paste("data",i,".txt", sep = "")
+    data[[i]] <- read.table(filename)
+    graph(data[[i]], filename)
+    probas <- probaErreur(data[[i]], 1, 0.5, 0.5)
+    cat(filename, "for p(test) = 1\n",
+        "\tproba erreur lda:", probas[1], "\n",
+        "\tproba erreur qda:", probas[2], "\n")
+
+    if(i %in% c(1,2)) {
+        for(p in c(1/4, 1/3, 1/2, 2/3, 3/4)) {
+            probaDF <- data.frame(trainingLDA = numeric(), 
+                                  trainingQDA = numeric(),
+                                  testLDA = numeric(),
+                                  testQDA = numeric())
+            for(j in 1:200) {
+                probaDF[j,] <- probaErreur(data[[i]], p, 0.5, 0.5)
+            }
+            cat(filename, "for p(test) =", p, "\n",
+                "\tproba erreur lda training:", mean(probaDF$trainingLDA), "\n",
+                "\tproba erreur qda training:", mean(probaDF$trainingQDA), "\n",
+                "\tproba erreur lda test:", mean(probaDF$testLDA), "\n",
+                "\tproba erreur qda test:", mean(probaDF$testQDA), "\n")
+        }
+        cat("\n")
+    } else {
+        for(j in 1:200) {
+            probaDF[j,] <- probaErreur(data[[i]], 2/3, 0.5, 0.5)
+        }
+        cat(filename, "for p(test) = 2/3\n",
+            "\tproba erreur lda training:", mean(probaDF$trainingLDA), "\n",
+            "\tproba erreur qda training:", mean(probaDF$trainingQDA), "\n",
+            "\tproba erreur lda test:", mean(probaDF$testLDA), "\n",
+            "\tproba erreur qda test:", mean(probaDF$testQDA), "\n\n")
+    }
 }
